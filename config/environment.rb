51508d71b4b91ef9c0273ffcf8497df12cf6afb4
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-04 12:14:33 -0700

    environment update from configure :deveopment to activerecord::base hash

--- config/environment.rb
+++ config/environment.rb
@@ -1,18 +1,15 @@
 ENV['SINATRA_ENV'] ||= 'development'
-# api_key = ENV['MY_SECRET']
+
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-
-configure :development do
-  set :database, 'sqlite:db/database.db'
-end
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+# configure :development do
+#   set :database, "sqlite:db/#{ENV['SINATRA_ENV']}.db"
+# end
 
 require './app/controllers/application_controller'
 require_all 'app'
-
-# environment is maintained on your system, not germaine to your app. Can be useful for security concerns, like an API key.
