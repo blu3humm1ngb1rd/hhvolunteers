5280c559e20a10841db5a3ad48ac3ecfb5bac96b
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-05 16:58:29 -0700

    add delete all seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
-kristen = Volunteer.create(firstname: 'Kristen', lastname: 'D', email: 'kristen@me.com', password: 'pass', pronouns: 'she/her', qtlgbt: 'true')
-eliseo = Volunteer.create(firstname: 'Eliseo', lastname: 'T', email: 'eliseo@me.com', password: 'pass', pronouns: 'they/them', qtlgbt: 'true', bipoc: 'true')
+Volunteer.delete_all
+
+kristen = Volunteer.create!(firstname: 'Kristen', lastname: 'D', email: 'kristen@me.com', password: 'pass', pronouns: 'she/her', qtlgbt: 'true')
+eliseo = Volunteer.create!(firstname: 'Eliseo', lastname: 'T', email: 'eliseo@me.com', password: 'pass', pronouns: 'they/them', qtlgbt: 'true', bipoc: 'true')
