cfe7b08073877e17ae44bae4b51935c232a8dc4f
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-05 08:22:39 -0700

    seeds typo email fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-kristen = Volunteer.create(firstname: 'Kristen', lastname: 'D', email: 'mel@mel.com', password: 'pass', pronouns: 'she/her', qtlgbt: 'true')
+kristen = Volunteer.create(firstname: 'Kristen', lastname: 'D', email: 'kristen@me.com', password: 'pass', pronouns: 'she/her', qtlgbt: 'true')
 eliseo = Volunteer.create(firstname: 'Eliseo', lastname: 'T', email: 'eliseo@me.com', password: 'pass', pronouns: 'they/them', qtlgbt: 'true', bipoc: 'true')
