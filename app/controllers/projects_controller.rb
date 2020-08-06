9d249cb64d64fc8955967166d2e1f7f8d2204751
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-06 12:46:08 -0700

    add basic functinoality to projects/show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,10 @@
 class ProjectsController < ApplicationController
   get '/projects/show' do
+    @project = Project.all
     erb :'projects/show'
+  end
+
+  get '/projects/show/:id' do
+    'this is projects speciifc page'
   end
 end
