5833172d43625f9da14dec941805b8e108c25302
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-06 06:24:34 -0700

    add authentication for user

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -4,34 +4,36 @@ class VolunteersController < ApplicationController
   end
 
   get '/login' do
+    @volunteer = Volunteer.new
     erb :login
   end
 
   post '/login' do
     @volunteer = Volunteer.find_by(email: params[:email])
-    # binding.pry
-    if @volunteer.authenticate(params[:password])
-      session[:volunteer_id] = @volunteer.id
-      redirect "volunteers/#{@volunteer.id}"
-    else
-      'Please check your email and password and try again'
-      redirect '/login'
+    if @volunteer
+      if @volunteer.authenticate(params[:password])
+        session[:volunteer_id] = @volunteer.id
+        redirect "volunteers/#{@volunteer.id}"
+      end
     end
+    @volunteer ||= Volunteer.new(params)
+    @volunteer.errors.add(:email, 'address and password do not match')
+    erb :'/login'
   end
 
   post '/volunteers' do
     # binding.pry
-    if params[:firstname] != '' && params[:email] != '' && params[:password] != ''
-      @volunteer = Volunteer.create(params)
+    @volunteer = Volunteer.new(volunteer_params)
+    if @volunteer.save
       session[:volunteer_id] = @volunteer.id
       redirect "volunteers/#{@volunteer.id}"
     else
-      'We need at least your first name, email, and a password for registration.'
-      redirect '/signup'
+      erb :'/signup'
     end
   end
 
   get '/volunteers/:id' do
+    redirect_if_logged_out
     @volunteer = Volunteer.find_by(id: params[:id])
     erb :'volunteers/show'
   end
@@ -48,6 +50,13 @@ class VolunteersController < ApplicationController
     session.clear
     redirect '/'
   end
+
+  private
+
+  def volunteer_params
+    params.select { |k, _v| %i[firstname lastname pronouns email training qtlgbt password bipoc].include?(k) }
+  end
 end
 
-# (:firstname params[:firstname], :lastname params[:lastname], :pronouns params[:pronouns],:email params[:email], :training params[:training], :qtlgbt params[:qtlgbt],:password params[:password], :bipoc params[:bipoc])
+# (firstname: params[:firstname], :lastname params[:lastname], :pronouns params[:pronouns],:email params[:email], :training params[:training], :qtlgbt params[:qtlgbt],:password params[:password], :bipoc params[:bipoc])
+# move colon to end of word
