db6fadcc3439d52628724447d685c7d670bfb0c6
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-06 12:26:07 -0700

    add 2 project seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 Volunteer.delete_all
+Project.delete_all
 
 kristen = Volunteer.create!(firstname: 'Kristen', lastname: 'D', email: 'kristen@me.com', password: 'pass', pronouns: 'she/her', qtlgbt: 'true')
 eliseo = Volunteer.create!(firstname: 'Eliseo', lastname: 'T', email: 'eliseo@me.com', password: 'pass', pronouns: 'they/them', qtlgbt: 'true', bipoc: 'true')
+
+pride = Project.create(name: 'Pride 2020', completed: 1, assigned: 1, role: 'coordinator', number_of_hours: 8)
+shefest = Project.create(name: 'Virtual SheFEST', completed: 1, assigned: 1, role: 'interpreter', number_of_hours: 8)
