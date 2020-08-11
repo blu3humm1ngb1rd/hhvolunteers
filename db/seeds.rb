4c952a70899d79c40cb3ec4f5ce1961bfca2b6c0
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-10 22:20:45 -0700

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@ kristen = Volunteer.create!(firstname: 'Kristen', lastname: 'D', email: 'kristen
 eliseo = Volunteer.create!(firstname: 'Eliseo', lastname: 'T', email: 'eliseo@me.com', password: 'pass', pronouns: 'they/them', qtlgbt: 'true', bipoc: 'true')
 
 pride = Project.create(name: 'Pride 2020', completed: 1, assigned: 1, role: 'coordinator', number_of_hours: 8, date: '9/20/20', time: '10a')
-shefest = Project.create(name: 'Virtual SheFEST', completed: 1, assigned: 1, role: 'interpreter', number_of_hours: 8, date: '11/1/20', time: '12p')
+shefest = Project.create(name: 'SheFEST', completed: 0, assigned: 0, role: 'interpreter', number_of_hours: 8, date: '11/1/20', time: '12p')
