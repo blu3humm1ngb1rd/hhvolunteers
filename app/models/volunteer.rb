8ed205edb31455c7adb2af6a63f1560b88009594
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-19 09:41:45 -0700

    add validations email firstname and password for volunteer

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,5 +1,9 @@
 class Volunteer < ActiveRecord::Base
+  validates_presence_of :email
   validates_uniqueness_of :email
+  validates_presence_of :firstname
+  validates_presence_of :password
+
   has_secure_password
   has_many :volunteer_projects
   has_many :projects, through: :volunteer_projects
