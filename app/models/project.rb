6407bbd4a0c8540a8ac7b10a7c5b1a75d8464b42
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-20 09:36:35 -0700

    add validations to project model and case sensitive to email

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
   has_many :volunteer_projects
   has_many :volunteers, through: :volunteer_projects
   belongs_to :creator, class_name: 'Volunteer', foreign_key: 'volunteer_id'
+
+  validates_presence_of :name
+  validates_presence_of :role
+  validates_presence_of :date
 end
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,6 +1,6 @@
 class Volunteer < ActiveRecord::Base
   validates_presence_of :email
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, case_sensitive: false
   validates_presence_of :firstname
   validates_presence_of :lastname
   has_secure_password
@@ -9,3 +9,5 @@ class Volunteer < ActiveRecord::Base
   has_many :projects, through: :volunteer_projects
   has_many :created_projects, class_name: 'Project'
 end
+
+# before_save : tell AR, before you save downcase
